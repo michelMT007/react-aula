1227731de983580db50d42794cbe9b11db9d4c22
Michel Alves <michelifmtads@gmail.com>
2021-01-16 08:53:05 -0400

    save

--- pages/page1.js
+++ pages/page1.js
@@ -9,7 +9,7 @@ function Page1(){
     }
     return(
         <div> 
-            <h1>Pagina 1 - Contador reatio Button</h1>
+            <h1>Pagina 1 - Contador reativo Button</h1>
             <h2>{contador}</h2>
             <button onClick={adicionaCont}>Click here!</button>   
          </div>
